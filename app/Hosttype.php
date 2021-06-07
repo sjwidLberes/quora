d8ba202a3391ea0831ef69b484661acd8cb37167
sjwidLberes <yurena19610407@126.com>
2021-06-07 15:02:58 +0800

    fix bugs

--- app/Hosttype.php
+++ app/Hosttype.php
@@ -8,6 +8,6 @@ class Hosttype extends Model
 {
     protected $table = 'host_types';
     public function hosts(){
-        return $this->hasMany('App\Host');
+        return $this->hasMany('App\Host','host_type_id','id');
     }
 }
